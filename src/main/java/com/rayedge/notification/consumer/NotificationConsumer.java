1a0d5631f64fac7cf365ee8adcc879a4e165bc80
Kiran <kshedge10@gmail.com>
2025-09-04 01:05:30 +0530

    service discovery

--- src/main/java/com/rayedge/notification/consumer/NotificationConsumer.java
+++ src/main/java/com/rayedge/notification/consumer/NotificationConsumer.java
@@ -1,15 +1,18 @@
 package com.rayedge.notification.consumer;
 
 import com.rayedge.notification.dto.NotificationRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class NotificationConsumer {
 
     @KafkaListener(topics = "notification-topic", groupId = "notification-group")
     public void consume(NotificationRequest request) {
         System.out.println("📩 Received notification: " + request.getMessage());
-        // Here you could call Email/SMS/Push notification logic
+        log.info("Notification received; message='{}'", request.getMessage());
+        // Here to add some call Email/SMS/Push notification logic
     }
 }
